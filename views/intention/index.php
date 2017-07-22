a42f593cef3f5cf5c18f681d9245cee3a90374a0
swebber <scott@teagan.org>
2017-07-22 12:56:57 -0400

    css cleanup

--- views/intention/index.php
+++ views/intention/index.php
@@ -68,7 +68,7 @@
     <div class="row">
         <div class="col-sm-12 text-center">
             <a href="/terra" class="btn btn-default">PREVIOUS</a>
-            <a href="/Intention>" class="btn btn-default">BACK TO TOP</a>
+            <a href="/intention" class="btn btn-default">BACK TO TOP</a>
             <a href="/chewy" class="btn btn-default">NEXT</a>
         </div>
     </div>
