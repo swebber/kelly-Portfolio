55f553bc585ac7eeb452f08e05c30249cacb21d8
swebber <scott@teagan.org>
2017-07-21 21:11:37 -0400

    update intention

--- views/intention/index.php
+++ views/intention/index.php
@@ -1,8 +1,74 @@
-<div class="intention">
+<div class="Intention portfolio-item">
+
+    <div class="row">
+        <div class="hidden-xs col-sm-6 col-md-offset-1 col-md-5">
+            <img class="img-responsive" src="/content/img/intention/intention-001.jpg" />
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-5">
+            2015
+            <h4>Intention</h4>
+            <p><i>Intention</i> exists as a concept for a quarterly magazine. <i>Intention</i> is not based on the belief that there is one correct way to live life, so is not intended to function as a rule book or a one-size-fits-all model. <i>Intention</i> is neither a doctrine nor a formulaic how-to guide; instead it aims to start conversations and encourage people, families, and organizations to name principles on which to base decisions, leading to a life lived with <i>Intention</i>. <i>Intention</i> believes that each person, family, and organization will require a unique set of guiding principles by which to live and operate. <i>Intention</i> provides uncommon resources and ideas that stray from the typical lifestyles portrayed within Western society. </p>
+        </div>
+        <div class="visible-xs-block col-xs-12">
+            <img class="img-responsive" src="/content/img/intention/intention-001.jpg" />
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-xs-12 col-sm-6 col-md-offset-1 col-md-5">
+            <img class="img-responsive" src="/content/img/intention/intention-002.jpg" />
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-5">
+            <img class="img-responsive" src="/content/img/intention/intention-003.jpg" />
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-12 col-md-offset-1 col-md-10">
+            <img class="img-responsive" src="/content/img/intention/intention-004.jpg" />
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-xs-12 col-sm-6 col-md-offset-1 col-md-5">
+            <img class="img-responsive" src="/content/img/intention/intention-005.jpg" />
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-5">
+            <img class="img-responsive" src="/content/img/intention/intention-006.jpg" />
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-xs-12 col-sm-6 col-md-offset-1 col-md-5">
+            <img class="img-responsive" src="/content/img/intention/intention-007.jpg" />
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-5">
+            <img class="img-responsive" src="/content/img/intention/intention-008.jpg" />
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-12 col-md-offset-1 col-md-10">
+            <img class="img-responsive" src="/content/img/intention/intention-009.jpg" />
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-xs-12 col-sm-4 col-md-offset-1 col-md-3">
+            <ul class="fa-ul">
+                <li>
+                    <i class="fa-li fa fa-plus"></i>
+                    Design, art direction, and photography were personally executed. All images for Intention were taken using Kodak color film.
+                </li>
+            </ul>
+        </div>
+        <div class="col-xs-12 col-sm-8 col-md-7">
+            Slideshow goes here...
+        </div>
+    </div>
     <div class="row">
         <div class="col-sm-12 text-center">
             <a href="/terra" class="btn btn-default">PREVIOUS</a>
-            <a href="/intention" class="btn btn-default">BACK TO TOP</a>
+            <a href="/Intention>" class="btn btn-default">BACK TO TOP</a>
             <a href="/chewy" class="btn btn-default">NEXT</a>
         </div>
     </div>
