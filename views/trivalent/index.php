9088e8932ed1d60c7c31cc93301d78b84b5f8643
swebber <scott@teagan.org>
2017-07-22 08:24:30 -0400

    added trivalent

--- views/trivalent/index.php
+++ views/trivalent/index.php
@@ -1,4 +1,49 @@
-<div class="meat">
+<div class="trivalent portfolio-item">
+
+    <div class="row">
+        <div class="hidden-xs col-sm-6 col-md-offset-1 col-md-5">
+            <img class="img-responsive" src="/content/img/trivalent/trivalent-001.png" />
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-5">
+            2016
+            <h4>TRIVALENT</h4>
+            <p>
+                Client: <a href="http://www.trivalent.co/" target="_blank">Trivalent</a><br />
+                With: <a href="http://drxlr.com/" target="_blank">Drexler</a>
+            </p>
+            <p>I designed multiple options for Trivalent's employee business card template. The designs expanded on a recent rebrand executed by Drexler. The final image represents their selected design.</p>
+        </div>
+        <div class="visible-xs-block col-xs-12">
+            <img class="img-responsive" src="/content/img/trivalent/trivalent-001.png" />
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-xs-12 col-sm-6 col-md-offset-1 col-md-5">
+            <img class="img-responsive" src="/content/img/trivalent/trivalent-002.png" />
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-5">
+            <img class="img-responsive" src="/content/img/trivalent/trivalent-003.png" />
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-xs-12 col-sm-4 col-md-offset-1 col-md-3">
+            <ul class="fa-ul">
+                <li>
+                    <i class="fa-li fa fa-plus"></i>
+                    Selected template design:
+                </li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-12 col-md-offset-1 col-md-10">
+            <img class="img-responsive" src="/content/img/trivalent/trivalent-004.png" />
+        </div>
+    </div>
+
     <div class="row">
         <div class="col-sm-12 text-center">
             <a href="/atlantic" class="btn btn-default">PREVIOUS</a>
@@ -6,4 +51,5 @@
             <a href="/california-tortilla" class="btn btn-default">NEXT</a>
         </div>
     </div>
+
 </div>
