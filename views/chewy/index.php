4fdd5979ad2bb04e8217380d79e927e20c3c3a1f
swebber <scott@teagan.org>
2017-07-21 21:35:36 -0400

    added chewy.com

--- views/chewy/index.php
+++ views/chewy/index.php
@@ -1,4 +1,53 @@
-<div class="chewy">
+<div class="chewy portfolio-item">
+
+    <div class="row">
+        <div class="col-xs-12 col-sm-6 col-md-offset-1 col-md-5">
+            2015
+            <h4>CHEWY.COM</h4>
+            <p>
+                Client: <a href="http://chewy.com/" target="_blank">Chewy.com</a><br />
+                With: <a href="http://drxlr.com/" target="_blank">Drexler</a>
+            </p>
+            <p>Work for Chewy.com started with devising design rules and strategies for an update on Chewy.com’s previous body of emails. Working alongside the team at Drexler, I tested and established a design plan for daily marketing emails and executed daily email designs.</p>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-12 col-md-offset-1 col-md-10">
+            <img class="img-responsive" src="/content/img/chewy/chewy-001.gif" />
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-12 col-md-offset-1 col-md-10">
+            <i class="fa fa-circle fullwidth" aria-hidden="true"></i>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-12 col-md-offset-1 col-md-10">
+            <img class="img-responsive" src="/content/img/chewy/chewy-002.jpg" />
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-12 col-md-offset-1 col-md-10">
+            <i class="fa fa-circle fullwidth" aria-hidden="true"></i>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-12 col-md-offset-1 col-md-10">
+            <img class="img-responsive" src="/content/img/chewy/chewy-003.jpg" />
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-12 col-md-offset-1 col-md-10">
+            <i class="fa fa-circle fullwidth" aria-hidden="true"></i>
+        </div>
+    </div>
+
     <div class="row">
         <div class="col-sm-12 text-center">
             <a href="/intention" class="btn btn-default">PREVIOUS</a>
@@ -6,4 +55,5 @@
             <a href="/waldens" class="btn btn-default">NEXT</a>
         </div>
     </div>
+
 </div>
