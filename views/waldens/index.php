e24c6df3ece8fb6f44dac846d9181024917cf586
swebber <scott@teagan.org>
2017-07-21 21:42:06 -0400

    added waldens

--- views/waldens/index.php
+++ views/waldens/index.php
@@ -1,4 +1,52 @@
-<div class="waldens">
+<div class="waldens portfolio-item">
+
+    <div class="row">
+        <div class="hidden-xs col-sm-6 col-md-offset-1 col-md-5">
+            <img class="img-responsive" src="/content/img/waldens/waldens-001.jpg" />
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-5">
+            2016
+            <h4>WALDEN'S</h4>
+            <p>Walden’s is the concept for a traditional roadside stand in rural Maine. In designing Walden’s packaging, the components were designed with the functionality of the stand in mind. Packaging needs to be customizable, accounting for the small scale production of the business. This explains my choice in allowing for product dates to be hand-written. This project provides a one-person operation with design that increases customer trust, while maintaining a casual sensibility.</p>
+        </div>
+        <div class="visible-xs-block col-xs-12">
+            <img class="img-responsive" src="/content/img/waldens/waldens-001.jpg" />
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-12 col-md-offset-1 col-md-10">
+            <img class="img-responsive" src="/content/img/waldens/waldens-002.jpg" />
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-12 col-md-offset-1 col-md-10">
+            <img class="img-responsive" src="/content/img/waldens/waldens-003.jpg" />
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-xs-12 col-sm-6 col-md-offset-1 col-md-5">
+            <img class="img-responsive" src="/content/img/waldens/waldens-004.jpg" />
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-5">
+            <img class="img-responsive" src="/content/img/waldens/waldens-005.jpg" />
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-12 col-md-offset-1 col-md-10">
+            <img class="img-responsive" src="/content/img/waldens/waldens-006.jpg" />
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-12 col-md-offset-1 col-md-10">
+            <img class="img-responsive" src="/content/img/waldens/waldens-007.jpg" />
+        </div>
+    </div>
+
     <div class="row">
         <div class="col-sm-12 text-center">
             <a href="/chewy" class="btn btn-default">PREVIOUS</a>
@@ -6,4 +54,5 @@
             <a href="/meat" class="btn btn-default">NEXT</a>
         </div>
     </div>
+
 </div>
