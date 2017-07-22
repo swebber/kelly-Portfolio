04cd354c0ee189905a495597323babddbc35dcd6
swebber <scott@teagan.org>
2017-07-21 21:48:02 -0400

    added meat

--- views/meat/index.php
+++ views/meat/index.php
@@ -1,4 +1,58 @@
-<div class="meat">
+<div class="meat portfolio-item">
+
+    <div class="row">
+        <div class="hidden-xs col-sm-6 col-md-offset-1 col-md-5">
+            <img class="img-responsive" src="/content/img/meat/meat-001.jpg" />
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-5">
+            2014
+            <h4>MEAT</h4>
+            <p>This book of infographics is not intended to persuade a person whether or not they should eat meat; rather, it is meant to present facts, both positive and negative, about the effects of meat consumption and production on human health and the environment. In order to engage the reader, I utilize infographics, icons, and minimal, conversational text.</p>
+        </div>
+        <div class="visible-xs-block col-xs-12">
+            <img class="img-responsive" src="/content/img/meat/meat-001.jpg" />
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-12 col-md-offset-1 col-md-10">
+            <img class="img-responsive" src="/content/img/meat/meat-002.jpg" />
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-12 col-md-offset-1 col-md-10">
+            <img class="img-responsive" src="/content/img/meat/meat-003.jpg" />
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-xs-12 col-sm-6 col-md-offset-1 col-md-5">
+            <img class="img-responsive" src="/content/img/meat/meat-004.jpg" />
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-5">
+            <img class="img-responsive" src="/content/img/meat/meat-005.jpg" />
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-12 col-md-offset-1 col-md-10">
+            <img class="img-responsive" src="/content/img/meat/meat-006.jpg" />
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-12 col-md-offset-1 col-md-10">
+            <img class="img-responsive" src="/content/img/meat/meat-007.jpg" />
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-sm-12 col-md-offset-1 col-md-10">
+            <img class="img-responsive" src="/content/img/meat/meat-008.jpg" />
+        </div>
+    </div>
+
     <div class="row">
         <div class="col-sm-12 text-center">
             <a href="/waldens" class="btn btn-default">PREVIOUS</a>
@@ -6,4 +60,5 @@
             <a href="/typographic" class="btn btn-default">NEXT</a>
         </div>
     </div>
+
 </div>
