1a14563cf570152c1bc5e1d9b1186ca7a01de38b
swebber <scott@teagan.org>
2017-08-07 21:52:47 -0400

    force css re-cache

--- views/layout.php
+++ views/layout.php
@@ -28,7 +28,7 @@ switch ($controller) {
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:400,500,700">
     <link rel="stylesheet" href="/content/font-awesome/css/font-awesome.min.css">
-    <link rel="stylesheet" href="/content/css/site.css">
+    <link rel="stylesheet" href="/content/css/site.css?v=001">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   </head>
